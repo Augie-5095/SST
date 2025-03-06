787d542a811a3ad57e64e68428cb728735b70b99
Augie Sorrentino <sorrentinoafs@gmail.com>
2025-03-06 09:01:13 -0800

    Added ability to overwrite file and insert text into it. No where near useable state

---
+++ textEditor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::ofstream file;
+    std::string fileName;
+    std::string input;
+
+    std::cout << "Please Enter the filename (example.txt)" << std::endl << ": ";
+    std::getline(std::cin, fileName);
+
+    file.open(fileName);
+    std::cout << fileName << " opened" << std::endl << ": ";
+    std::getline(std::cin, input);
+
+    file << input + "\n";
+    file.close();
+
+    return 0;
+}
