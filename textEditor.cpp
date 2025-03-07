ef16703f0891ec588ee27acdfa8a4c00fbb4c126
Augie Sorrentino <sorrentinoafs@gmail.com>
2025-03-07 09:07:28 -0800

    Added commandline arugment support

--- textEditor.cpp
+++ textEditor.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <fstream>
 
-int main()
+int main(int argc, char *argv[])
 {
     std::ofstream file;
     std::string fileName;
     std::string input;
 
-    std::cout << "Please Enter the filename (example.txt)" << std::endl << ": ";
-    std::getline(std::cin, fileName);
+    if (argc < 2)
+    {
+        std::cerr << "Error: Argument expected" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <string>" << std::endl;
+    }
+
+    fileName = argv[1];
 
     file.open(fileName, std::ios_base::app);
     std::cout << fileName << " opened" << std::endl << ": ";
